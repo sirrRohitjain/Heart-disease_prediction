0cbe4ae4d64272e8202ed6889d642a3b565c401f
sirrRohitjain <rohitjainrj1983@gmail.com>
2026-03-30 16:23:51 +0530

    dataset  is ready

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
 
-class A:
-    a = 5
-    def __init__(self):
-        print("This is a class A")
+keras.utils.set_random_seed(42)
+df = pd.read_csv("http://storage.googleapis.com/download.tensorflow.org/data/heart.csv")
+df.to_csv("dataset.csv", index=False)
 
-b = A()
-print(b.a)
-print("hh")
+
+
+
